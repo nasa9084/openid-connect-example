25976850214db4a5f8745f934a9ec5664aaebba7
nasa9084 <nasa9084@live.jp>
2017-09-25 11:05:44 +0900

    initial commit

---
+++ openidprovider/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+const (
+	iss       = "https://localhost:8080"
+	authnHTML = `<html>
+<body>
+AuthN?<br />
+
+<form action="/authorize?%s" method="POST">
+<label>ID: <input name="id" placeholder="userid"></label>
+<label>Password: <input type="password" name="passwd" placeholder="passwd"></label>
+<input type="submit">
+</form>
+</body>
+</html>`
+	authzHTML = `<html>
+<body>
+AuthZ?<br />
+<a href="/authorize/yes?%s">YES</a>
+<a href="/authorize/no?%s">NO</a>
+</body>
+</html>`
+	tokenJSON = `{
+  "access_token": "somethingToken",
+  "token_type": "Bearer",
+  "id_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJzdWIiOiIyNDQwMDMyMCIsImF1ZCI6ImNsaWVudCBhcHBsaWNhdGlvbiIsImV4cCI6MTMxMTI4MTk3MCwiaWF0IjoxMzExMjgwOTcwfQ.HPWARNhL212vwkH8FAJQ073rU9hoVHasZNQi0y-qkR5e_SyIIQ7a5Li5q6iyGf4UZfsjCwkdbRdW67PQiTMiuSXnE-8KKAyN1lo68RaqTK8Vkc58d6aq8jWyZs1FRIEFK2eG83mSrJP08rHQyzx2iRBtM3BX3kzPec2VaCxjUaQ",
+}`
+)
+
+func main() { os.Exit(exec()) }
+
+func exec() int {
+	// binding routes
+	http.Handle(`/authorize`, logMiddleware(authzHandler))
+	http.Handle(`/authenticate`, logMiddleware(authnHandler))
+	http.Handle(`/authorize/yes`, logMiddleware(authzYesHandler))
+	http.Handle(`/authorize/no`, logMiddleware(authzNoHandler))
+	http.Handle(`/token`, logMiddleware(tokenHandler))
+
+	// run http server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+	return 0
+}
+
+func logMiddleware(h http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf(`REQUEST: %s`, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func authzHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case `GET`:
+		valid := true
+		if !isOpenIDConnect(r.FormValue(`scope`)) {
+			valid = false
+		}
+		responseType := r.FormValue(`response_type`)
+		if responseType == "" {
+			valid = false
+		}
+		clientID := r.FormValue(`client_id`)
+		if clientID == "" {
+			valid = false
+		}
+		redirectURI := r.FormValue(`redirect_uri`)
+		if redirectURI == "" {
+			valid = false
+		}
+		if !valid {
+			w.Write([]byte(`this is not valie OpenID Connect Request`))
+			return
+		}
+
+		w.Header().Set(`Location`, `/authenticate?`+r.Form.Encode())
+		w.WriteHeader(http.StatusFound)
+	case `POST`:
+		if !authenticate(r.FormValue(`id`), r.FormValue(`passwd`)) {
+			// if the user authn is failed
+			w.Header().Set(`Location`, `authenticate?`+r.URL.Query().Encode())
+			w.WriteHeader(http.StatusFound)
+		}
+		// if the user authn is succeeded
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(authzHTML, r.URL.Query().Encode(), r.URL.Query().Encode())))
+	}
+}
+
+func authnHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(authnHTML, r.URL.Query().Encode())))
+}
+
+func authzYesHandler(w http.ResponseWriter, r *http.Request) {
+	query := url.Values{}
+	query.Add(`code`, `authorizedyes`)
+	w.Header().Set(`Location`, r.FormValue(`redirect_uri`)+`?`+query.Encode())
+	w.WriteHeader(http.StatusFound)
+}
+
+func authzNoHandler(w http.ResponseWriter, r *http.Request) {
+	query := url.Values{}
+	query.Add(`error`, `access_denied`)
+	w.Header().Set(`Location`, r.FormValue(`redirect_uri`)+`?`+query.Encode())
+	w.WriteHeader(http.StatusFound)
+}
+
+// if production, you must use TLS to access to this endpoint
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set(`Content-Type`, `application/json`)
+	w.Header().Set(`Cache-Control`, `no-store`)
+	w.Header().Set(`Pragma`, `no-cache`)
+	code := r.FormValue(`code`)
+	if code != "authorizedyes" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "invalid_request"}`))
+		return
+	}
+	w.Write([]byte(tokenJSON))
+}
+
+func isOpenIDConnect(scope string) bool {
+	if scope == "" {
+		return false
+	}
+	if scope == "openid" {
+		return true
+	}
+	scopeList := strings.Split(scope, ",")
+	if len(scopeList) < 2 {
+		return false
+	}
+	for _, scope = range scopeList {
+		if scope == "openid" {
+			return true
+		}
+	}
+	return false
+}
+
+func authenticate(userid, password string) bool {
+	return userid == `userid` && password == `passwd`
+}
---
+++ relyingparty/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+const (
+	iss       = "https://localhost:8000"
+	indexHTML = `<html>
+<body>
+<a href="/authz">Start AuthZ</a>
+</body>
+</html>`
+	callbackHTML = `<html>
+<body>
+token: %s
+</body>
+</html>`
+)
+
+func main() { os.Exit(exec()) }
+
+func exec() int {
+	// binding routes
+	http.Handle(`/`, logMiddleware(indexHandler))
+	http.Handle(`/authz`, logMiddleware(authzHandler))
+	http.Handle(`/callback`, logMiddleware(callbackHandler))
+
+	// run http server
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+
+	return 0
+}
+
+func logMiddleware(h http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf(`REQUEST: %s`, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(indexHTML))
+}
+
+func authzHandler(w http.ResponseWriter, r *http.Request) {
+	query := url.Values{}
+	query.Add(`scope`, `openid`)
+	query.Add(`response_type`, `code`)
+	query.Add(`client_id`, `client application`)
+	query.Add(`redirect_uri`, `http://localhost:8000/callback`)
+	w.Header().Set(`Location`, `http://localhost:8080/authorize?`+query.Encode())
+	w.WriteHeader(http.StatusFound)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	form := url.Values{}
+	form.Add(`grant_type`, `authorization_code`)
+	code := r.FormValue(`code`)
+	if code == "" {
+		w.Write([]byte(fmt.Sprintf(`<html><body>Error: %s</body></html>`, r.FormValue(`error`))))
+	}
+	form.Add(`code`, code)
+	form.Add(`redirect_uri`, `http://localhost:8000/callback`)
+	buf := bytes.Buffer{}
+	buf.WriteString(form.Encode())
+	// in production, you must use https
+	resp, _ := http.Post(`http://localhost:8080/token`, `application/x-www-form-urlencoded`, &buf)
+	// you must validate response id token in production
+	tokenb, _ := ioutil.ReadAll(resp.Body)
+	token := string(tokenb)
+
+	w.Write([]byte(fmt.Sprintf(callbackHTML, token)))
+}
